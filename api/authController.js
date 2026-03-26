9c508fc8bc6ccddc9072870949f432b18c213166
Zeenah-Yusuf <zeedexx699@gmail.com>
2026-03-26 20:15:12 +0100

    Add payment and verification actions to auth handler

--- api/authController.js
+++ api/authController.js
@@ -1,1 +1,18 @@
+import { initiatePayment } from "./interswitchPayment";
+import { sendVerificationCode } from "./whatsappVerification";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).end();
+
+  const { action } = req.body;
+
+  if (action === "login") {
+    return await sendVerificationCode(req, res);
+  }
+  if (action === "checkout") {
+    return await initiatePayment(req, res);
+  }
+
+  res.status(400).json({ error: "Invalid action" });
+}
 
